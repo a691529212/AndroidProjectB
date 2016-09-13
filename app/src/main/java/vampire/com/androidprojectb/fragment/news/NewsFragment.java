42efd404857b6e7eb283ce42d37abfe469fc1360
任飞宇 <835499883@qq.com>
2016-09-13 10:48:49 +0800

    no message

--- app/src/main/java/vampire/com/androidprojectb/fragment/news/NewsFragment.java
+++ app/src/main/java/vampire/com/androidprojectb/fragment/news/NewsFragment.java
@@ -1,8 +1,39 @@
 package vampire.com.androidprojectb.fragment.news;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+
+import vampire.com.androidprojectb.R;
+import vampire.com.androidprojectb.base.BaseFragment;
+import vampire.com.androidprojectb.fragment.news.adapter.NewsAdapter;
+
 /**
  * Created by Vampire on 16/9/12.
  */
-public class NewsFragment {
+public class NewsFragment extends BaseFragment{
     private static final String TAG = "Vampire_NewsFragment";
+    private TabLayout mNewsTabLayout;
+    private ViewPager mNewsViewPager;
+    private NewsAdapter newsAdapter;
+
+
+    @Override
+    protected int setLayout() {
+        return R.layout.fragment_news;
+    }
+
+    @Override
+    protected void initView() {
+        mNewsTabLayout = bindView(R.id.fragment_news_tablayout);
+        mNewsViewPager = bindView(R.id.fragment_news_viewpager);
+
+
+    }
+
+    @Override
+    protected void initData() {
+        newsAdapter= new NewsAdapter(getChildFragmentManager());
+        mNewsViewPager.setAdapter(newsAdapter);
+        mNewsTabLayout.setupWithViewPager(mNewsViewPager);
+    }
 }
