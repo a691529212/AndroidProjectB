3fb9123f5d6c1ae5873d5c042b172431db067254
vampire <691529212@qq.com>
2016-09-13 09:20:51 +0800

    no message

--- app/src/main/java/vampire/com/androidprojectb/MainActivity.java
+++ app/src/main/java/vampire/com/androidprojectb/MainActivity.java
@@ -1,11 +1,31 @@
 package vampire.com.androidprojectb;
 
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import vampire.com.androidprojectb.base.BaseActivity;
+import vampire.com.androidprojectb.fragment.news.NewsFragment;
+import vampire.com.androidprojectb.fragment.recreation.RecreationFragment;
+import vampire.com.androidprojectb.fragment.topic.TopicFragment;
+import vampire.com.androidprojectb.fragment.user.UserFragment;
 
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements View.OnClickListener {
+
+
+    private FrameLayout frameLayout;
+    private RadioButton btnNews;
+    private RadioButton btnRecreation;
+    private RadioButton btnTopic;
+    private RadioButton btnUser;
+    private RadioGroup radioGroup;
+    private FragmentManager manager;
 
 
     @Override
@@ -16,10 +36,66 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void initView() {
 
+        frameLayout = bindView(R.id.frame_layout_main);
+
+        radioGroup = bindView(R.id.rg_main);
+
+        btnNews = bindView(R.id.btn_news);
+        btnRecreation = bindView(R.id.btn_recreation);
+        btnTopic = bindView(R.id.btn_topic);
+        btnUser = bindView(R.id.btn_user);
+
+        radioGroup.check(R.id.btn_news);
+
+        btnNews.setOnClickListener(this);
+        btnRecreation.setOnClickListener(this);
+        btnTopic.setOnClickListener(this);
+        btnUser.setOnClickListener(this);
+
+        manager =getSupportFragmentManager();
+
     }
 
     @Override
     protected void initData() {
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+            // 新闻
+            case R.id.btn_news:
+                NewsFragment newsFragment = new NewsFragment();
+                upDataFragment(newsFragment);
+                break;
+
+            // 娱乐
+            case R.id.btn_recreation:
+                RecreationFragment recreationFragment =new RecreationFragment();
+                upDataFragment(recreationFragment);
+                break;
+
+            // 话题
+            case R.id.btn_topic:
+                TopicFragment topicFragment =new TopicFragment();
+                upDataFragment(topicFragment);
+                break;
+
+            // 我
+            case R.id.btn_user:
+                UserFragment userFragment =new UserFragment();
+                upDataFragment(userFragment);
+                break;
+        }
+
+    }
+
+    // 替换fragment
+    private void upDataFragment(Fragment fragment){
+
+        FragmentTransaction transaction =  manager.beginTransaction();
+        transaction.replace(R.id.frame_layout_main,fragment).commit();
     }
 }
--- app/src/main/java/vampire/com/androidprojectb/fragment/user/UserFragment.java
+++ app/src/main/java/vampire/com/androidprojectb/fragment/user/UserFragment.java
@@ -1,8 +1,26 @@
 package vampire.com.androidprojectb.fragment.user;
 
+import vampire.com.androidprojectb.R;
+import vampire.com.androidprojectb.base.BaseFragment;
+
 /**
  * Created by Vampire on 16/9/12.
  */
-public class UserFragment {
+public class UserFragment extends BaseFragment{
     private static final String TAG = "Vampire_UserFragment";
+
+    @Override
+    protected int setLayout() {
+        return R.layout.fragment_user;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
 }
